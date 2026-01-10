909cd75bf8f988a7245531d9f90e9071190bde86
Dinesh <dineshingale2003@gmail.com>
2026-01-10 16:37:49 +0530

    fix(test): set window size to prevent screenshot cutoff (closes #36)

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -8,6 +8,7 @@ def test_dashboard_access():
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=chrome_options)
     
